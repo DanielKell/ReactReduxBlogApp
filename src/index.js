6503025792e5480436a3a19f4f59855459a5ee40
DanielKell <danielkell.ubc@gmail.com>
2017-10-02 20:16:07 -0700

    Added a basic react router that loads different components based on paths

--- src/index.js
+++ src/index.js
@@ -2,14 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+//Route object/component provides config options to react-router
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+  render() {
+    return <div>Hello!</div> 
+  }
+}
+
+class Goodbye extends React.Component {
+  render() {
+    return <div>Goodbye!</div>
+  }
+}
+//Go to THIS path, and load THIS component
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello}/> 
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
