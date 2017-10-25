ba4fe3d8f5625efdedc0da8263a29641c46058de
DanielKell <danielkell.ubc@gmail.com>
2017-10-24 22:58:52 -0700

    Hook up a basic Redux Form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,34 @@
 import React, {Component} from 'react';
+import { Field, reduxForm } from 'redux-form'; //Reduxform is similar to connect!
 
 class PostsNew extends Component {
-    render() {
-        return (
+    
+    renderTitleField(field) { //this field object contains event handlers we need for the JSX!
+        //If we put an input inside this return, how does the <Field> below know to connect to it?
+        //It doesn't - until we use the above field object event handlers!
+         return (
             <div>
-                Posts New!
+                <input
+                    type="text"
+                    {...field.input} //Pull all the event handlers out
+                />
             </div>
+         );
+    }
+
+    render() {
+
+        return (
+            <form>
+                <Field
+                    name="title" //What piece of state are we editing?
+                    component={this.renderTitleField} //Function to display this component
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm' //If we show multiple forms, Redux will handle it correctly with this
+}) (PostsNew); //The string must be unique.
